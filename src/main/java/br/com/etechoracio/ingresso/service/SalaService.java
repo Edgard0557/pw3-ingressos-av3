ea064a207b94b6445a34a8fda4ab6d0ba2609522
Igor e Gustavo Rodrigues <i.lopck08@gmail.com>
2026-09-22 15:26:20 -0300

    feat: Etapa 1: listagem de todas as salas ativas (GET /salas)

---
+++ src/main/java/br/com/etechoracio/ingresso/service/SalaService.java
@@ -0,0 +1,28 @@
+package br.com.etechoracio.ingresso.service;
+
+import br.com.etechoracio.ingresso.dto.SalaResponseDTO;
+import br.com.etechoracio.ingresso.entity.Sala;
+import br.com.etechoracio.ingresso.mapper.SalaMapper;
+import br.com.etechoracio.ingresso.repository.SalaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalaService {
+
+    @Autowired
+    private SalaRepository salaRepository;
+
+    @Autowired
+    private SalaMapper salaMapper;
+
+    public List<SalaResponseDTO> listarSalasAtivas() {
+        List<Sala> salas = salaRepository.findByDataExclusaoIsNull();
+
+        return salaMapper.toRespostaDTOList(salas);
+    }
+
+
+}
